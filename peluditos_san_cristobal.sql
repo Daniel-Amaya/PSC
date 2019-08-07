a843576f681646233922b08ad9d9a48a8a7f9e9f
Daniel Amaya <danytf2103@gmail.com>
2019-08-07 11:22:08 -0500

    Base de datos

---
+++ peluditos_san_cristobal.sql
@@ -0,0 +1,379 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-08-2019 a las 18:20:40
+-- Versión del servidor: 10.3.16-MariaDB
+-- Versión de PHP: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `peluditos_san_cristobal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `adopciones`
+--
+
+CREATE TABLE `adopciones` (
+  `numAdopcion` int(10) UNSIGNED NOT NULL,
+  `idAnimalAdoptado` int(10) UNSIGNED NOT NULL,
+  `idUsuario` int(10) UNSIGNED NOT NULL,
+  `codEsterilizacion` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `animales`
+--
+
+CREATE TABLE `animales` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `especie` varchar(30) NOT NULL,
+  `raza` varchar(25) NOT NULL,
+  `color` varchar(30) NOT NULL,
+  `sexo` char(1) NOT NULL,
+  `esterilizado` tinyint(1) NOT NULL,
+  `descripcion` tinyblob NOT NULL,
+  `procedencia` varchar(30) NOT NULL,
+  `carpeta` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `animales`
+--
+
+INSERT INTO `animales` (`id`, `nombre`, `especie`, `raza`, `color`, `sexo`, `esterilizado`, `descripcion`, `procedencia`, `carpeta`) VALUES
+(1, 'Perry', 'perro', 'pitbull', 'cafÃ©', 'M', 1, 0x6b6c73646a66666b6c6a6c6b7364, 'MedellÃ­n Villa SofÃ­a', 'Perry113814625'),
+(2, 'Perry', 'pitbull', 'cafÃ©', 'si', 'P', 0, 0x6a686b686b686b6a, 'agregar', 'Perry57966764'),
+(3, 'Perry', 'Gato', 'pitbull', 'cafÃ©', 'F', 1, 0x44534664, 'MedellÃ­n Villa SofÃ­a', 'Perry93818741'),
+(4, 'Perry', 'Gato', 'pitbull', 'cafÃ©', 'F', 0, 0x4a736b6168646a6b736164, 'MedellÃ­n Villa SofÃ­a', 'Perry89260501');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `animalesvacunados`
+--
+
+CREATE TABLE `animalesvacunados` (
+  `codVacuna` int(10) UNSIGNED NOT NULL,
+  `idAnimal` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `apadrinamientos`
+--
+
+CREATE TABLE `apadrinamientos` (
+  `idUsuario` int(10) UNSIGNED NOT NULL,
+  `idAnimal` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `citas`
+--
+
+CREATE TABLE `citas` (
+  `numCita` int(10) UNSIGNED NOT NULL,
+  `fechaHora` datetime NOT NULL,
+  `idUsuario` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compromisoesterilizacion`
+--
+
+CREATE TABLE `compromisoesterilizacion` (
+  `cod` int(10) UNSIGNED NOT NULL,
+  `fechaEstipulada` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fotos`
+--
+
+CREATE TABLE `fotos` (
+  `cod` int(10) UNSIGNED NOT NULL,
+  `direccion` varchar(30) NOT NULL,
+  `idAnimal` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `preguntasadopcion`
+--
+
+CREATE TABLE `preguntasadopcion` (
+  `numPregunta` int(10) UNSIGNED NOT NULL,
+  `pregunta` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `preguntasadopcion`
+--
+
+INSERT INTO `preguntasadopcion` (`numPregunta`, `pregunta`) VALUES
+(1, 0xc2bf506f72207175c3a92064657365612061646f7074617220756e61206d6173636f74613f),
+(2, 0xc2bf41637475616c6d656e7465207469656e65206f74726f7320616e696d616c69746f733f),
+(3, 0xc2bf5369206c6f73207469656e652c20657374c3a16e206573746572696c697a61646f733f),
+(4, 0x416e746572696f726d656e74652068612074656e69646f206f74726f7320616e696d616c69746f733f),
+(5, 0xc2bf5175c3a920667565206c6f2071756520706173c3b320636f6e20c3a96c2f656c6c6f733f),
+(6, 0xc2bf457374c3a1206465206163756572646f20656e20717565207365206861676120756e61207669736974612070657269c3b364696361206120737520646f6d6963696c696f20706172612076657220636f6d6f20736520656e6375656e74726120656c20616e696d616c69746f2061646f707461646f3f),
+(7, 0xc2bf4375616e74617320706572736f6e617320766976656e20656e20737520636173613f),
+(8, 0xc2bf457374c3a16e20746f646f73206465206163756572646f20656e2061646f707461723f),
+(9, 0xc2bf486179206e69c3b16f7320656e20636173613f),
+(10, 0xc2bf416c677569656e20717565207669766120636f6e207573746564657320657320616cc3a9726769636f2061206c6f7320616e696d616c6573206f2073756672652064652061736d613f),
+(11, 0x456e206361736f2064652061716c75696c65722c20c2bf53757320617272656e6461646f726573207065726d6974656e20616e696d616c69746f7320656e206c612063617361206f206170617274616d656e746f3f),
+(12, 0x536920706f7220616c67c3ba6e206d6f7469766f2074757669657261207175652063616d6269617220646520646f6d6963696c696f2c20c2bf5175c3a9207061736172c3ad6120636f6e20656c20616e696d616c69746f3f);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuestasadopcion`
+--
+
+CREATE TABLE `respuestasadopcion` (
+  `cod` int(10) UNSIGNED NOT NULL,
+  `respuesta` blob NOT NULL,
+  `numPregunta` int(10) UNSIGNED NOT NULL,
+  `idUsuario` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `apellidos` varchar(30) NOT NULL,
+  `correo` varchar(30) NOT NULL,
+  `telefono` varchar(11) NOT NULL,
+  `cedula` varchar(15) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vacunas`
+--
+
+CREATE TABLE `vacunas` (
+  `cod` int(10) UNSIGNED NOT NULL,
+  `especie` varchar(30) NOT NULL,
+  `vacuna` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `adopciones`
+--
+ALTER TABLE `adopciones`
+  ADD PRIMARY KEY (`numAdopcion`),
+  ADD KEY `fk_adopcionesAnimales` (`idAnimalAdoptado`),
+  ADD KEY `fk_adopcionesUsuarios` (`idUsuario`),
+  ADD KEY `fk_adopcionesEsterilizacion` (`codEsterilizacion`);
+
+--
+-- Indices de la tabla `animales`
+--
+ALTER TABLE `animales`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `animalesvacunados`
+--
+ALTER TABLE `animalesvacunados`
+  ADD KEY `fk_codVacuna` (`codVacuna`),
+  ADD KEY `fk_idAnimal` (`idAnimal`);
+
+--
+-- Indices de la tabla `apadrinamientos`
+--
+ALTER TABLE `apadrinamientos`
+  ADD KEY `fk_USUAPA` (`idUsuario`),
+  ADD KEY `fk_ANIAPA` (`idAnimal`);
+
+--
+-- Indices de la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD PRIMARY KEY (`numCita`),
+  ADD KEY `fk_citasUsuarios` (`idUsuario`);
+
+--
+-- Indices de la tabla `compromisoesterilizacion`
+--
+ALTER TABLE `compromisoesterilizacion`
+  ADD PRIMARY KEY (`cod`);
+
+--
+-- Indices de la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  ADD PRIMARY KEY (`cod`),
+  ADD KEY `fk_ANFOT` (`idAnimal`);
+
+--
+-- Indices de la tabla `preguntasadopcion`
+--
+ALTER TABLE `preguntasadopcion`
+  ADD PRIMARY KEY (`numPregunta`);
+
+--
+-- Indices de la tabla `respuestasadopcion`
+--
+ALTER TABLE `respuestasadopcion`
+  ADD PRIMARY KEY (`cod`),
+  ADD KEY `fk_preguntaRespuesta` (`numPregunta`),
+  ADD KEY `fk_usuarioRespuesta` (`idUsuario`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `vacunas`
+--
+ALTER TABLE `vacunas`
+  ADD PRIMARY KEY (`cod`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `adopciones`
+--
+ALTER TABLE `adopciones`
+  MODIFY `numAdopcion` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `animales`
+--
+ALTER TABLE `animales`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `citas`
+--
+ALTER TABLE `citas`
+  MODIFY `numCita` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `compromisoesterilizacion`
+--
+ALTER TABLE `compromisoesterilizacion`
+  MODIFY `cod` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  MODIFY `cod` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `preguntasadopcion`
+--
+ALTER TABLE `preguntasadopcion`
+  MODIFY `numPregunta` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `respuestasadopcion`
+--
+ALTER TABLE `respuestasadopcion`
+  MODIFY `cod` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `vacunas`
+--
+ALTER TABLE `vacunas`
+  MODIFY `cod` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `adopciones`
+--
+ALTER TABLE `adopciones`
+  ADD CONSTRAINT `fk_adopcionesAnimales` FOREIGN KEY (`idAnimalAdoptado`) REFERENCES `animales` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_adopcionesEsterilizacion` FOREIGN KEY (`codEsterilizacion`) REFERENCES `compromisoesterilizacion` (`cod`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_adopcionesUsuarios` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `animalesvacunados`
+--
+ALTER TABLE `animalesvacunados`
+  ADD CONSTRAINT `fk_codVacuna` FOREIGN KEY (`codVacuna`) REFERENCES `vacunas` (`cod`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_idAnimal` FOREIGN KEY (`idAnimal`) REFERENCES `animales` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `apadrinamientos`
+--
+ALTER TABLE `apadrinamientos`
+  ADD CONSTRAINT `fk_ANIAPA` FOREIGN KEY (`idAnimal`) REFERENCES `animales` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_USUAPA` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD CONSTRAINT `fk_citasUsuarios` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  ADD CONSTRAINT `fk_ANFOT` FOREIGN KEY (`idAnimal`) REFERENCES `animales` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `respuestasadopcion`
+--
+ALTER TABLE `respuestasadopcion`
+  ADD CONSTRAINT `fk_preguntaRespuesta` FOREIGN KEY (`numPregunta`) REFERENCES `preguntasadopcion` (`numPregunta`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_usuarioRespuesta` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
