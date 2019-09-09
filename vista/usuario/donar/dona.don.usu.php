2f1bfa169f0c93f7f50d8acb1f646b69a6fc65bb
Daniel Amaya <danytf2103@gmail.com>
2019-09-08 22:05:59 -0500

    pantallas donaciones general y administrador

--- vista/usuario/donar/dona.don.usu.php
+++ vista/usuario/donar/dona.don.usu.php
@@ -1,68 +1,247 @@
 <section class='margin-menu padding-menu'>
 
-<h1 class="titulo">Hacer una donación</h1>
+<h1 class="titulo">¿Que desea donar?</h1>
 
-<fieldset class=" fielNewAnimalito" style="">
 
-<h2 class="center">Agregar una nueva mascota</h2>
+        <div class="containerCat">
+        
+        <div class="plane">
+            
+            <div class="p-body">
+            <div class="p-parts">
+                <div class="part-one">
+                <span class="po-up"></span>
+                <span class="po-middle"></span>
+                <span class="po-down"></span>
+                </div>
+                <div class="part-two">
+                <span class="pt-wing"></span>
+                </div>
+                <div class="part-three">
+                <span class="roll"></span>
+                </div>
+                <div class="helix">
+                <span class="h-base"></span>
+                <span class="h-rotor"></span>
+                </div>
+            </div>
+            
+            <div class="cat">
+                <span class="eyes left"></span>
+                <span class="eyes right"></span>
+                <span class="mouth"></span>
+            </div>
+            </div>
+            
+            <div class="p-message">
+            <div class="rope"></div>
+            <ul>
+                <li class='tabsItemUser'> <strong>Dinero</strong></li>
+                <li class="middle tabsItemUser"> <strong>Medicamentos</strong></li>
+                <li class='tabsItemUser'> <strong> Alimento</strong></li>
+            </ul>
+            </div>
+        </div>
+        
+        </div>
 
-<form action="" method="post" class='newAnimalito' id='newAnimalito'>
-    <div class="boxInput">
-        <input type="text" placeholder="nombre del animalito" name='nombreAn'>
-    </div>
+      
+<div class="container-donar" >
+        <div class="col-6 margin-menu grisColor" style="margin-left:15px;">
+           
 
-    <div class="boxInput">
-        <select name="especie">
-            <option value="">Especie</option>
-            <option value="Perro">Perro</option>
-            <option value="Gato">Gato</option>
-        </select>
-        <!-- <input type="text" placeholder="especie" name='especie'> -->
-    </div>
+            <h2 class="center">Formulario de donación</h2>
+            <br>
+            
 
-    <div class="boxInput">
-        <input type="text" placeholder="raza" name='raza'>
-    </div>
+            <form action="" method="post" id=''>
+            
+            <h3>¿De que es su donación?</h3>
+                <div class="boxInput">
+                    <select name="opciones"  required> 
+                        <!-- <option disabled selected>Opciones</option> -->
+                        <option></option>
+                        <option value="alimentos">Alimentos</option>
+                        <option value="medicamentos">Medicamentos</option>
+                        <option value="dinero">Dinero</option>
+                    </select>
+                   
+                </div>
 
-    <div class="boxInput">
-        <input type="text" placeholder="color" name='color'>
-    </div>
+            <h3>¿De cuanto es su donacion?</h3>
+                <div class="boxInput"  >
+                    <input type="number" placeholder="Cantidad" name=''  required>
+                </div>
 
-    <div class="boxInput">
-        <select name="sexo">
-            <option value="">Sexo</option>
-            <option value="M">Masculino</option>
-            <option value="F">Femenino</option>
-        </select>
-    </div>
+                <div class="boxInput" style="width:60%" >
+                    <select name="opciones" required>
+                        <!-- <option disabled selected>Medición</option> -->
+                        <option></option>
+                        <option value="libras">Lb/Libras</option>
+                        <option value="unidades">Unidades</option>
+                        <option value="pesos">$/Pesos</option>
+                    </select>
+                   
+                </div>
+                <div class="boxInput">
+                        <label for="nuevaFoto" class="btn_gris">Agregar foto:</label>
+                        <input type="file" name="fotoComprovante" id="fotoComprovante" class="none" accept="image/*">
+                        
+                    </div>
 
-    <div class="boxInput">
-        <input type="number" name="edad" placeholder='Año de nacimiento'>
-    </div>
+                 <div id="imagesBox">
+                        <h3 style="margin-left:15px;">Imagenes agregadas:</h3>
+                        <div id="messageError"></div>
+                </div>
+        
+                <div class="boxInput">
+                    <textarea name="mensajeDonacion" placeholder="Si tienes algo que decirnos escribelo aquí" rows="4"></textarea>
+                </div>
 
-    <div class="boxInput">
-        <h3>¿Esterilizado?</h3>
-        <label for="si">Sí</label>
-        <input type="radio" name="esterilizado" id="si" value='si'>
-        <label for="no">No</label>
-        <input type="radio" name='esterilizado' id='no' value='no'>
-    </div>
 
-    <div class="boxInput">
-        <textarea name="descripcion" placeholder="descripcion del animalito" rows="4"></textarea>
-    </div>
+                <div class="boxInputGris ">
+                    <input type="submit" value="Enviar" style="">
+                </div>
 
-    <div class="boxInput">
-        <input type="text" name='procedencia' placeholder="procedencia">
-    </div>
+            </form>
+                
+        
 
-    <div class="boxInput">
-        <input type="submit" value="agregar">
-    </div>
+        </div>
 
-</form>
+        <div class="col-6">
+        <div class="tabBox">
+                <div class="textos-modal donacionesAlimentos">
+
+                <h1>Donaciones</h1> <br>
+            <div class="doscolumnas">
+                    <p><h3>¿Que es donar?</h3> <br>
+                    Donar es dar algo de valor para ayudar al bienestar del otro 
+                    entregandose  de forma voluntaria y sin esperar premio ni recompensa alguna. <br>
+                    <h3> ¿Para que donar? </h3> <br>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero accusamus blanditiis ducimus sapiente asperiores voluptate ad, </p>
+            </div> <br>
+                        <h3>  ¿Como puedo donar? </h3> <br>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore consequuntur ea provident alias blanditiis quas eum? Magni quas nesciunt sed accusamus esse architecto, reprehenderit quos suscipit cumque minima libero dicta?</p> 
+                    </p>
+                    <br>
+            <div class="containerquedonar">
+                <div class="quedonar1"> 
+                    <img src="publico/images/donar.jpg" class="quedonarCont">
+                    <p class="quedonarCont"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, non! Libero, </p>
+                </div>
     
-</fieldset>
+            </div> <br>
+                
+                <p>
+                    <div style="width: 90%; height: 3px; background-color: Orange; Margin:auto; margin-top: 3px;"></div>
+                    </p>
+                </div>
+
+        </div>
 
 
-</section>
+
+    
+
+
+ <div class="tabBox">
+ <div class="textos-modal donacionesAlimentos">
+
+                <h1>Donaciones2</h1> <br>
+                <div class="doscolumnas">
+                    <p><h3>¿Que es donar?</h3> <br>
+                    Donar es dar algo de valor para ayudar al bienestar del otro 
+                    entregandose  de forma voluntaria y sin esperar premio ni recompensa alguna. <br>
+                    <h3> ¿Para que donar? </h3> <br>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero accusamus blanditiis ducimus sapiente asperiores voluptate ad, </p>
+                </div> <br>
+                        <h3>  ¿Como puedo donar? </h3> <br>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore consequuntur ea provident alias blanditiis quas eum? Magni quas nesciunt sed accusamus esse architecto, reprehenderit quos suscipit cumque minima libero dicta?</p> 
+                    </p>
+                    <br>
+                <div class="containerquedonar">
+                <div class="quedonar1"> 
+                    <img src="publico/images/donar.jpg" class="quedonarCont">
+                    <p class="quedonarCont"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, non! Libero, </p>
+                </div>
+
+                </div> <br>
+
+                <p>
+                    <div style="width: 90%; height: 3px; background-color: Orange; Margin:auto; margin-top: 3px;"></div>
+                    </p>
+                </div>
+
+    
+    </div>
+    <div class="tabBox">
+    <div class="textos-modal donacionesAlimentos">
+
+            <h1>Donaciones3 </h1> <br>
+            <div class="doscolumnas">
+                <p><h3>¿Que es donar?</h3> <br>
+                Donar es dar algo de valor para ayudar al bienestar del otro 
+                entregandose  de forma voluntaria y sin esperar premio ni recompensa alguna. <br>
+                <h3> ¿Para que donar? </h3> <br>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero accusamus blanditiis ducimus sapiente asperiores voluptate ad, </p>
+            </div> <br>
+                    <h3>  ¿Como puedo donar? </h3> <br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore consequuntur ea provident alias blanditiis quas eum? Magni quas nesciunt sed accusamus esse architecto, reprehenderit quos suscipit cumque minima libero dicta?</p> 
+                </p>
+                <br>
+            <div class="containerquedonar">
+            <div class="quedonar1"> 
+                <img src="publico/images/donar.jpg" class="quedonarCont">
+                <p class="quedonarCont"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, non! Libero, </p>
+            </div>
+
+            </div> <br>
+
+            <p>
+                <div style="width: 90%; height: 3px; background-color: Orange; Margin:auto; margin-top: 3px;"></div>
+                </p>
+            </div>
+
+</div>
+</div>
+</div>
+</div>
+</section> 
+
+<script>
+tabsUser();
+function tabsUser(){
+    var tabButtons = classNames('tabsItemUser');
+    var tabs = classNames('tabBox');
+
+    var indic = 0;
+
+    for(let i = 0; i < tabButtons.length; i++){
+
+        tabs[i].style.display = 'none';
+
+        tabButtons[i].addEventListener('click', function(){
+        for(let e = 0; e < tabButtons.length; e++){
+            tabs[e].style.display = 'none';
+            tabButtons[e].classList.remove('tabSelection');
+        }
+
+            indic = i;
+            tabs[indic].style.display = 'block';
+            tabButtons[indic].classList.add('tabSelection');
+
+        });
+
+
+    }
+
+    tabs[indic].style.display = "block";
+    tabButtons[indic].classList.add('tabSelection');
+
+
+
+}
+
+</script>
+
