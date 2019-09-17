4255418950c8b0b6a4cce465f3878ee17669043d
ElMosque <jdmm360@gmail.com>
2019-09-16 20:26:01 -0500

    Cosas simples
    - Se arreglaron cosas simples

--- vista/usuario/adoptar/animales.ad.php
+++ vista/usuario/adoptar/animales.ad.php
@@ -1,5 +1,5 @@
 <div class="padding-menu margin-menu">
-    <h3 class="titulo">Conoce a la mascota que deseas adoptar <i class='fas fa-search' style='float: right'></i></h3> 
+    <h3 class="titulo">Conoce a la mascota que deseas adoptar <i class='fas fa-search buscador-mascotas'></i></h3> 
         <?php 
             require_once 'controlador/get/adoptarDataUser.php';
         
